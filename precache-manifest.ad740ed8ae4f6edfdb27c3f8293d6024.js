0cdc5babbd357d7e088db856cfcd139c51b6346a
Maanvi Maini <maanvi20>
2019-10-10 17:00:02 +0530

    Updates

---
+++ precache-manifest.ad740ed8ae4f6edfdb27c3f8293d6024.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f10912a2259bba786d22872efa76beb",
+    "url": "/myfirstprojectwhitepanda/index.html"
+  },
+  {
+    "revision": "a6d0946307c1af62d1a5",
+    "url": "/myfirstprojectwhitepanda/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "dfd8e25be1d4121b1cc0",
+    "url": "/myfirstprojectwhitepanda/static/css/main.85af4405.chunk.css"
+  },
+  {
+    "revision": "a6d0946307c1af62d1a5",
+    "url": "/myfirstprojectwhitepanda/static/js/2.b8d453a3.chunk.js"
+  },
+  {
+    "revision": "dfd8e25be1d4121b1cc0",
+    "url": "/myfirstprojectwhitepanda/static/js/main.db0b28f8.chunk.js"
+  },
+  {
+    "revision": "93ec6b3807d10d2fccb3",
+    "url": "/myfirstprojectwhitepanda/static/js/runtime-main.369d45cc.js"
+  },
+  {
+    "revision": "483f6b5683140e7df7f19482adcdb7fe",
+    "url": "/myfirstprojectwhitepanda/static/media/avatar.483f6b56.png"
+  },
+  {
+    "revision": "bb350d86c0609f89d737c720f3307b2d",
+    "url": "/myfirstprojectwhitepanda/static/media/bell.bb350d86.png"
+  }
+]);
